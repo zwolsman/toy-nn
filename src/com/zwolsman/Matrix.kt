aa6495929d8beae79c22430d8dc0f5b064218a7c
Marvin <marvin.zwolsman@gmail.com>
2018-04-30 15:55:26 +0200

    Written multiply tests

--- src/com/zwolsman/Matrix.kt
+++ src/com/zwolsman/Matrix.kt
@@ -1,6 +1,13 @@
 package com.zwolsman
 
 class Matrix(val rows: Int, val cols: Int, generator: (Int, Int) -> Int = Matrix.defaultGenerator) {
+    operator fun times(scalar: Int): Matrix {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    operator fun times(m2: Matrix): Matrix {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     companion object {
         val defaultGenerator = { _: Int, _: Int ->
             0
@@ -12,5 +19,10 @@ class Matrix(val rows: Int, val cols: Int, generator: (Int, Int) -> Int = Matrix
             generator(x, y)
         }
     }
+
+    constructor(arr: Array<Array<Int>>) : this(arr.size, arr.maxBy { it.size }?.max() ?: 0, { x, y ->
+        arr[x][y]
+    })
+
 
 }
--- test/com/zwolsman/MatrixTest.kt
+++ test/com/zwolsman/MatrixTest.kt
@@ -2,6 +2,7 @@ package com.zwolsman
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.lang.UnsupportedOperationException
 
 internal class MatrixTest {
 
@@ -15,6 +16,33 @@ internal class MatrixTest {
             for(y in 0 until cols) {
                 assertEquals(0, m.data[x][y])
             }
+        }
+    }
+    
+    @Test
+    fun `Multiply a matrix by a scalar`() {
+        val matrix = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
+        val scalar = 1
+        val result = matrix * scalar
+        assertEquals(matrix.data, result.data)
+    }
+
+    @Test
+    fun `Multiply a matrix by a matrix element wise`() {
+        val m1 = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
+        val m2 = Matrix(arrayOf(arrayOf(5,6), arrayOf(7,8)))
+        val result = m1 * m2
+        val target = Matrix(arrayOf(arrayOf(6, 12), arrayOf(21, 32)))
+        assertEquals(target.data, result.data)
+
+    }
+
+    @Test
+    fun `Multiply a matrix by a matrix non matching elements throws`() {
+        val m1 = Matrix(arrayOf(arrayOf(1), arrayOf(3)))
+        val m2 = Matrix(arrayOf(arrayOf(5,6), arrayOf(7,8)))
+        assertThrows(UnsupportedOperationException::class.java) {
+            val result = m1 * m2
         }
     }
 }
