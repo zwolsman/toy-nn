a80bad5f3bb2eb183f23cb90bb4694c8a9fb8141
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-04-30 15:33:41 +0200

    Matrix inits with 0

--- test/com/zwolsman/MatrixTest.kt
+++ test/com/zwolsman/MatrixTest.kt
@@ -1,7 +1,20 @@
 package com.zwolsman
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class MatrixTest {
-    
+
+    @Test
+    fun `Default generator inits with all 0's`() {
+        val rows = 2
+        val cols = 5
+        val m = Matrix(rows, cols)
+
+        for (x in 0 until rows) {
+            for(y in 0 until cols) {
+                assertEquals(0, m.data[x][y])
+            }
+        }
+    }
 }
