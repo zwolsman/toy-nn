4f46e8c7e87e09c53f000616f38459b6aa4e6c45
Marvin <marvin.zwolsman@gmail.com>
2018-04-30 17:33:05 +0200

    Upgrading matrix to use doubles

--- test/com/zwolsman/MatrixTest.kt
+++ test/com/zwolsman/MatrixTest.kt
@@ -2,7 +2,6 @@ package com.zwolsman
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.math.exp
 
 internal class MatrixTest {
 
@@ -14,33 +13,33 @@ internal class MatrixTest {
 
         for (x in 0 until rows) {
             for(y in 0 until cols) {
-                assertEquals(0, m.data[x][y])
+                assertEquals(0.0, m.data[x][y])
             }
         }
     }
 
     @Test
     fun `Multiply a matrix by a scalar`() {
-        val matrix = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
-        val scalar = 1
+        val matrix = Matrix(arrayOf(arrayOf(1.0,2.0), arrayOf(3.0,4.0)))
+        val scalar = 1.0
         val result = matrix * scalar
         assertArrayEquals(matrix.data, result.data)
     }
 
     @Test
     fun `Multiply a matrix by a matrix element wise`() {
-        val m1 = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
-        val m2 = Matrix(arrayOf(arrayOf(5,6), arrayOf(7,8)))
+        val m1 = Matrix(arrayOf(arrayOf(1.0,2.0), arrayOf(3.0,4.0)))
+        val m2 = Matrix(arrayOf(arrayOf(5.0,6.0), arrayOf(7.0,8.0)))
         val result = m1 * m2
-        val target = Matrix(arrayOf(arrayOf(5, 12), arrayOf(21, 32)))
+        val target = Matrix(arrayOf(arrayOf(5.0, 12.0), arrayOf(21.0, 32.0)))
         assertArrayEquals(target.data, result.data)
 
     }
 
     @Test
     fun `Multiply a matrix by a matrix non matching elements throws`() {
-        val m1 = Matrix(arrayOf(arrayOf(1), arrayOf(3)))
-        val m2 = Matrix(arrayOf(arrayOf(5,6), arrayOf(7,8)))
+        val m1 = Matrix(arrayOf(arrayOf(1.0), arrayOf(3.0)))
+        val m2 = Matrix(arrayOf(arrayOf(5.0,6.0), arrayOf(7.0,8.0)))
         assertThrows(IllegalStateException::class.java) {
             val result = m1 * m2
         }
@@ -48,26 +47,26 @@ internal class MatrixTest {
 
     @Test
     fun `Add scalar to matrix`() {
-        val matrix = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
-        val scalar = 0
+        val matrix = Matrix(arrayOf(arrayOf(1.0,2.0), arrayOf(3.0,4.0)))
+        val scalar = 0.0
         val result = matrix + scalar
         assertArrayEquals(matrix.data, result.data)
     }
 
     @Test
     fun `Add element wise`() {
-        val m1 = Matrix(arrayOf(arrayOf(1,2), arrayOf(3,4)))
-        val m2 = Matrix(arrayOf(arrayOf(5,6), arrayOf(7,8)))
+        val m1 = Matrix(arrayOf(arrayOf(1.0,2.0), arrayOf(3.0,4.0)))
+        val m2 = Matrix(arrayOf(arrayOf(5.0,6.0), arrayOf(7.0,8.0)))
         val result = m1 + m2
-        val target = Matrix(arrayOf(arrayOf(6, 8), arrayOf(10, 12)))
+        val target = Matrix(arrayOf(arrayOf(6.0, 8.0), arrayOf(10.0, 12.0)))
         assertArrayEquals(target.data, result.data)
     }
 
     @Test
     fun `Transpose matrix`() {
-        val m1 = Matrix(arrayOf(arrayOf(1,2,3), arrayOf(3,4,5)))
+        val m1 = Matrix(arrayOf(arrayOf(1.0,2.0,3.0), arrayOf(3.0,4.0,5.0)))
         val result = m1.transpose()
-        val expected = Matrix(arrayOf(arrayOf(1,3), arrayOf(2,4), arrayOf(3,5)))
+        val expected = Matrix(arrayOf(arrayOf(1.0,3.0), arrayOf(2.0,4.0), arrayOf(3.0,5.0)))
 
         assertArrayEquals(expected.data, result.data)
     }
